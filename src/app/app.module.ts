714f5f985d39bd9cb68419aea87898124462208d
satveerbhantoo <satveerbhantoo011@gmail.com>
2018-09-11 19:57:36 +0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,91 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Camera } from '@ionic-native/camera';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { Items } from '../mocks/providers/items';
+import { Settings, User, Api } from '../providers';
+import { MyApp } from './app.component';
+
+
+
+//firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBU8tU4oN4FgBIhpL2ozE2_HiDlzkbNYhs",
+  authDomain: "krakathon2018.firebaseapp.com",
+  databaseURL: "https://krakathon2018.firebaseio.com",
+  projectId: "krakathon2018",
+  storageBucket: "krakathon2018.appspot.com",
+  messagingSenderId: "881083451568"
+}
+
+// The translate loader needs to know where to load i18n files
+// in Ionic's static asset pipeline.
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export function provideSettings(storage: Storage) {
+  /**
+   * The Settings provider takes a set of default settings for your app.
+   *
+   * You can add new settings options at any time. Once the settings are saved,
+   * these values will not overwrite the saved values (this can be done manually if desired).
+   */
+  return new Settings(storage, {
+    option1: true,
+    option2: 'Ionitron J. Framework',
+    option3: '3',
+    option4: 'Hello'
+  });
+}
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    Api,
+    Items,
+    User,
+    Camera,
+    SplashScreen,
+    StatusBar,
+    { provide: Settings, useFactory: provideSettings, deps: [Storage] },
+    // Keep this to enable Ionic's runtime error handling during development
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AngularFireDatabase,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule { }
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+import { User } from '../../providers';
+import { MainPage } from '../';
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html'
+})
+export class SignupPage {
+  // The account fields for the login form.
+  // If you're using the username field with or without email, make
+  // sure to add it to the type
+  account: { name: string, email: string, password: string } = {
+    name: 'Test Human',
+    email: 'test@example.com',
+    password: 'test'
+  };
+
+  // Our translated text strings
+  private signupErrorString: string;
+  private AngularFireTest: any;
+
+  constructor(public navCtrl: NavController, 
+    NgFire: AngularFireDatabase,
+    public user: User,
+    public toastCtrl: ToastController,
+    public translateService: TranslateService) {
+
+    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
+      this.signupErrorString = value;
+    })
+
+    this.AngularFireTest = NgFire.object('test').valueChanges();
+    
+
+  }
+
+  doSignup() {
+    // Attempt to login in through our User service
+    this.user.signup(this.account).subscribe((resp) => {
+      this.navCtrl.push(MainPage);
+    }, (err) => {
+
+      this.navCtrl.push(MainPage);
+
+      // Unable to sign up
+      let toast = this.toastCtrl.create({
+        message: this.signupErrorString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    });
+  }
+}
